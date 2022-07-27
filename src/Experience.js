247026e81779cfc098334b129106afdbf1b955d8
disha4091 <disha4091@gmail.com>
2022-07-28 00:42:54 +0530

    new changes

--- src/Experience.js
+++ src/Experience.js
@@ -2,15 +2,25 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div class="container about" id="experience"> 
+    <div class="container " id="experience"> 
     <h1>Experience</h1>
-    <div class="aboutContainer row">
+    <div className="aboutContainer">
+    <div class=" row">
+      <div class=" col-md">
+        <h2>Product Intern at Adobe</h2>
+        <p>Worked on implementation of virtual background feature in Adobe Connect App. Image segmentation using MediaPipe library and opencv to perform masking of image frame in real time video
+stream.</p>
+      </div>
+    </div>
+    <div class="row">
       <div class="col-md">
         <h2>Web development Intern at Qwings</h2>
         <p>Worked on the development of frontend and backend of the edtech
 platform. Created responsive pages from figma designs. Added profile picture feature along with login and signup page. Implemented features like sound effects, flashcards, animations, etc. </p>
       </div>
     </div>
+    </div>
+    
 </div>
   )
 }
